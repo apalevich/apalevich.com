6a2bad87ebd86e12ccb059cea65442b901582275
apalevich <backstageofme@gmail.com>
2023-11-03 11:22:42 +0400

    Change port as per SO instruction

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const mysql = require('mysql');
 const app = express();
 
 // Set the port for the server to listen on
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON and urlencoded data
 app.use(bodyParser.json());
