5299ce164c1d3ff5387bfed1a4f72a173826ee01
apalevich <backstageofme@gmail.com>
2024-02-24 13:30:08 +0400

    Add GoogleAnalytics

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apalevich.com',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   devToolbar: {
     enabled: false
   }
