396bc4fea7f0b69a328f77f36a6846d24ac2d1ab
apalevich <backstageofme@gmail.com>
2024-02-26 14:54:09 +0400

    Remove partyRown artefacts

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apalevich.com',
   integrations: [
     tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
   devToolbar: {
     enabled: false
